2ec115e6faba4cf49609b5b6f94d10700b277600
Saurabh Sharma <saurabh.dev.work@gmail.com>
2020-07-07 22:13:21 +0530

    Updates

---
+++ precache-manifest.4b2cec16d9ed75a56e579b6c2ad82c30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659dc54fa92e2b0e54e220e2795c9551",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "0fc0a73b5ee0fdbbbdf6",
+    "url": "/react-todo-app/static/css/2.d53c156d.chunk.css"
+  },
+  {
+    "revision": "ad733c8bc17158519751",
+    "url": "/react-todo-app/static/css/main.45f74ef5.chunk.css"
+  },
+  {
+    "revision": "0fc0a73b5ee0fdbbbdf6",
+    "url": "/react-todo-app/static/js/2.5c041a4e.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react-todo-app/static/js/2.5c041a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad733c8bc17158519751",
+    "url": "/react-todo-app/static/js/main.03ed43d8.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/saurabhvedwal/react-todo-app.git/precache-manifest.c7bd25c681242db13efc1e4f6a6d8916.js"
+  "/react-todo-app/precache-manifest.4b2cec16d9ed75a56e579b6c2ad82c30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saurabhvedwal/react-todo-app.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
