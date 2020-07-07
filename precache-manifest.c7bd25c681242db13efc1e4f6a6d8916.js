b7b27b03519d5bae0d01687fcb43708ffc18759a
Saurabh Sharma <saurabh.dev.work@gmail.com>
2020-07-07 22:06:54 +0530

    Updates

---
+++ precache-manifest.c7bd25c681242db13efc1e4f6a6d8916.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6fdcf5643cbbb30265aedfae31051f",
+    "url": "/saurabhvedwal/react-todo-app.git/index.html"
+  },
+  {
+    "revision": "a9beb0352d4e4c48e05c",
+    "url": "/saurabhvedwal/react-todo-app.git/static/css/2.d53c156d.chunk.css"
+  },
+  {
+    "revision": "b816e7d67afb3f081906",
+    "url": "/saurabhvedwal/react-todo-app.git/static/css/main.45f74ef5.chunk.css"
+  },
+  {
+    "revision": "a9beb0352d4e4c48e05c",
+    "url": "/saurabhvedwal/react-todo-app.git/static/js/2.50a756d0.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/saurabhvedwal/react-todo-app.git/static/js/2.50a756d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b816e7d67afb3f081906",
+    "url": "/saurabhvedwal/react-todo-app.git/static/js/main.a81e5a3d.chunk.js"
+  },
+  {
+    "revision": "79311ff5a4613bd55067",
+    "url": "/saurabhvedwal/react-todo-app.git/static/js/runtime-main.0702ccea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/saurabhvedwal/react-todo-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
